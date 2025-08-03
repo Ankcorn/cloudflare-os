5db417edff06063168b98ccc1a5072f84e29d8e1
Kenton Varda <kenton@cloudflare.com>
2025-08-03 18:50:06 -0500

    Populate workshop-shared.
    Moves adapter.ts (renamed to gatekeeper.ts).
    
    Adds api.ts with frontend<->backend API.

---
+++ packages/workshop-shared/src/api.ts
@@ -0,0 +1,139 @@
+// This file defines the API spoken between the Minions Workshop service and the front-end UI.
+//
+// The UI is a good old "fat client" SPA. Why not use SSR? Because:
+// - Users of this UI are likely to have it open often, maybe even all the time. Startup time is
+//   less of a concern than with sites you visit only briefly, and assets are likely to be in cache
+//   in any case.
+// - The Minions themselves are sandboxed on the client side in addition to the server side. This
+//   sandboxing requires running code in the browser. It is not plausible to server-side render
+//   a Minion itself.
+// - By providing a really clean API boundary between client and server, we make it easier to build
+//   alternative clients.
+// - SPA is just easier to think about.
+//
+// The entire API between the client and server is an RPC API, using Cloudflare's JavaScript RPC,
+// which essentially allows natural JavaScript / TypeScript interfaces to be exposed over the
+// network.
+//
+// The RPC interface operates over a WebSocket, which the client starts immediately at startup and
+// keeps open for the entire lifetime of the session, reconnecting if needed.
+//
+// Minions run inside a sandboxed iframe which has no ability to talk to the outside world at all,
+// except postMessage() to the parent frame. Through postMessage() exchanges, the Minion can speak
+// RPC to the Workshop. Among other things, through this interface, the Workshop provides the
+// Minion a stub pointing to the Minion's server-side Durable Object interface.
+
+import { RpcStub } from "jsrpc";
+
+// Public API exposed to the internet.
+interface PublicApi {
+  // Authenticates the user using an auth token (typically stored in localStorage).
+  //
+  // TODO: Is this right for Cloudflare Access?
+  authenticate(token: string): Promise<AuthenticatedApi>;
+}
+
+// Top-level API exposed to the user after they have authenticated.
+interface AuthenticatedApi {
+  // Open an existing minion.
+  openMinion(id: string): Promise<Overseer | null>;
+
+  // Create a new minion. It will start out titled "Untitled Minion".
+  newMinion(): Promise<Overseer>;
+
+  // List metadata about all the user's Minions. Used to display the front-page listing.
+  //
+  // TODO: Pagination, sort options.
+  listMinions(): Promise<MinionMetadata[]>;
+
+  // TODO: Configure adapters
+}
+
+// Metadata about a Minion. Includes everything needed to render the Minion list on the front
+// page.
+type MinionMetadata = {
+  // Unique ID for this Minion, used with `openMinion()`. This is a url-safe base64 value chosen
+  // randomly when the Minion is created.
+  id: string;
+
+  // Human-readable title. Can be modified.
+  title: string;
+
+  // TODO:
+  // - owner, shared-with
+  // - created / modified / activity times
+  // - icon? thumbnail?
+}
+
+// Describes the client-side UI code for a Minion. Such code is intended to run inside an iframe
+// sandbox with no access to the outside world except through an RPC interface to the Workshop
+// and to the Minion's server.
+type UiCode = {
+  // URL from which the main bundle of UI code can be downloaded. This download contains all the
+  // Minion's client-side assets. The URL is content-addressed to make it highly cacheable, even
+  // across multiple Minions sharing the same implementation (blueprint).
+  //
+  // TODO: Specify the format of what this URL returns. A raw HTML page doesn't quite work because
+  //   the client needs to initialize the sandbox with some platform libraries before loading the
+  //   Minion itself.
+  url: string;
+
+  // Other metadata could be placed here in the future, e.g. to specify what version of support
+  // libraries should be loaded.
+};
+
+// Interface to a Minion's Overseer, used to display the Minion Workshop shell UI around that
+// Minion.
+interface Overseer {
+  // Get metadata describing this minion.
+  getMetadata(): Promise<MinionMetadata>;
+
+  // Change the title.
+  setTitle(title: string): Promise<void>;
+
+  // Get the Minion's deployed UI code, to be run inside an iframe sandbox.
+  //
+  // Returns null if the minion has no deployed UI code (e.g. if it's new, or if it's just an AI
+  // agent with no code).
+  getUiCode(): Promise<UiCode | null>;
+
+  // Open an RPC interface to the Minion's server-side Durable Object facet. The frontend may pass
+  // this stub into the Minion's iframe sandbox, so that the Minion UI can communicate with its
+  // server side. It can also permit the coding agent to make direct calls.
+  connectToMinion(): RpcStub<any>;
+
+  // List all the Minion's current gatekeepers.
+  listGatekeepers(): Promise<GatekeeperMetadata[]>;
+
+  // Get an existing gatekeeper by binding name.
+  getGatekeeper(bindingName: string): Promise<GatekeeperClient<any> | null>;
+
+  // Try to create a new gatekeeper for this URL. A binding name will be automatically assigned.
+  newGatekeeper(resourceUrl: string): Promise<GatekeeperClient<any> | null>;
+
+  // TODO:
+  // - Code editing API
+  // - Agent chat API
+  // - View action queue
+  //   - Approve actions
+  // - Sharing / access control functions
+}
+
+// Information about one of a Minion's gatekeepers, for the purpose of displaying it in a list.
+type GatekeeperMetadata = {
+  bindingName: string;
+  resourceTitle: string;
+};
+
+interface GatekeeperClient<Session> {
+  // Get and set the binding name.
+  getBindingName(): Promise<string>;
+  setBindingName(name: string): Promise<void>;
+
+  // Get the resource description, including the schema of its RPC interface.
+  describe(): Promise<ResourceDescription>;
+
+  // Open a direct session to this gatekeeper. Particularly useful when using the AI agent to talk
+  // to the resource directly.
+  openSession(): Promise<Session>;
+}
